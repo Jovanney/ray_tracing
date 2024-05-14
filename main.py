e5ad7751d040ea4e555e240a7d4704581286f0bd
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-05-13 23:15:29 -0300

    feat: creating the  main file with initial tests
    this commit provides the main file with the initialization of a camera, a plane and an sphere. it calls the ray casting method over entidades array

---
+++ main.py
@@ -0,0 +1,34 @@
+"""Main File"""
+
+from vectors import Ponto, Vetor
+from entidades import Esfera, Plane
+from camera import Camera
+
+
+def main():
+    """Main Function"""
+
+    camera = Camera(
+        target=Ponto(0, 0, 0),
+        position=Ponto(0, 0, 10),
+        up=Vetor(0, 1, 0),
+    )
+
+    plano = Plane(
+        point=Ponto(0, 0, 0),
+        normal=Vetor(0, 0, 1),
+        color=(255, 255, 255),
+    )
+
+    esfera = Esfera(
+        center=Ponto(0, 0, 0),
+        radius=1,
+        color=(255, 0, 0),
+    )
+
+    entidades = [plano, esfera]
+
+    camera.__ray_casting__(entidades)
+
+
+main()
