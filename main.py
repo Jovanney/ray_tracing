b7bc47b67e1640902d2b956658f8ffbee9933f61
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-05-16 00:52:25 -0300

    refactor: Update Camera class resolution parameters to improve rendering quality

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ def main():
     """Main Function"""
 
     camera = Camera(
-        target=Ponto(0, 0, 0),
-        position=Ponto(0, 0, 10),
+        target=Ponto(2, 0, 0),
+        position=Ponto(0, 1, 0),
         up=Vetor(0, 1, 0),
     )
 
     plano = Plane(
-        point=Ponto(0, 0, 0),
-        normal=Vetor(0, 0, 1),
+        point=Ponto(0, 1, 0),
+        normal=Vetor(0, -1, 0),
         color=(255, 0, 0),
     )
 
     esfera = Esfera(
-        center=Ponto(0, 0, 0),
-        radius=1,
-        color=(255, 0, 0),
+        center=Ponto(2, 0, 0),
+        radius=0.25,
+        color=(0, 128, 0),
     )
 
     entidades = [plano, esfera]
