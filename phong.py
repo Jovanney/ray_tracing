cabeaffc1ffc7fa0872a6857aaa6147ea2e08afe
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-06-20 20:04:54 -0300

    refactor: Update Phong lighting model function
    This commit updates the `phong_model` function to include additional properties for the coefficients of diffuse, specular, ambient, reflection, and transmission lighting effects, as well as the roughness coefficient. These properties enhance the accuracy of lighting calculations and improve the overall functionality of the function.

--- phong.py
+++ phong.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def phong_model():
+# od é a cor do objeto
+def phong_model(od):
     # Coeficientes de iluminação
     kd = np.array(
         [0.7, 0.7, 0.7]
@@ -9,14 +10,14 @@ def phong_model():
     ks = np.array(
         [0.5, 0.5, 0.5]
     )  # especular é o efeito de iluminação que faz com que a luz se espalhe em um ponto
-    ka = np.array([0.1, 0.1, 0.1])  # ambiental
-    kr = np.array([0.5, 0.5, 0.5])  # reflexão
-    kt = np.array([0.5, 0.5, 0.5])  # transmissão
-    n = 10  # coeficiente de rugosidade
+    ka = np.array([0.1, 0.1, 0.1])  # atenuador da luz ambiente
+    kr = np.array([0.5, 0.5, 0.5])  # reflexão, o quão reflexivo ele é
+    kt = np.array([0.5, 0.5, 0.5])  # transmissão, o quão transmissivo ele é
+    n = 10  # coeficiente de rugosidade, o qual rugoso ele é (afeta a especularidade)
 
     # Cores
-    Ia = np.array([255, 255, 255])
-    Il = np.array([255, 255, 255])
+    Ia = np.array([255, 255, 255])  # Intensisade da luz (luz branca)
+    Il = np.array([255, 255, 255])  # Intensidade da luz (luz branca)
 
     # Vetores
     L = np.array([1, 1, 1])
@@ -24,9 +25,11 @@ def phong_model():
     V = np.array([1, 1, 1])
     R = np.array([1, 1, 1])
 
-    # Cálculo da intensidade
-    Id = kd * Il * max(0, np.dot(L, N))
-    Is = ks * Il * max(0, np.dot(V, R)) ** n
-    Ia = ka * Ia
-    I = Id + Is + Ia
+    # ka * Ia é o ambiente (luz ambiente)
+    # Para cada ponto de luz nós iremos rodar a parte da direita da equação
+    # Il * od * kd * (N.dot(L)) componente difusa
+    # Il * ks * (R.dot(V)) ** n componente especular
+
+    I = ka * Ia + (Il * od * kd * (N.dot(L)) + Il * ks * (R.dot(V)) ** n)
+
     return I
