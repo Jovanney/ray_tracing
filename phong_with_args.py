d74a9cea16088e719907014b681d484283e9bc7b
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-06-26 21:59:48 -0300

    refactor: Normalize surface normals in phong_with_args.py

--- phong_with_args.py
+++ phong_with_args.py
@@ -1,3 +1,5 @@
+"""Módulo para cálculo de iluminação usando o modelo de Phong."""
+
 import numpy as np
 
 from entidades import Esfera, Plane, Mesh
@@ -5,6 +7,19 @@ from vectors import Ponto
 
 
 def phong(entidade, luzes, ponto_intersec, camera_position):
+    """
+    Calcula a cor resultante em um ponto de interseção usando o modelo de Phong.
+
+    Args:
+        entity: A entidade (Esfera, Plano ou Malha) na qual a luz incide.
+        lights: Lista de fontes de luz que incidem sobre a entidade.
+        intersection_point: Ponto de interseção da luz com a entidade.
+        camera_position: Posição da câmera.
+
+    Returns:
+        Uma lista representando a cor RGB resultante no ponto de interseção.
+    """
+
     Ia = np.array([255, 255, 255])  # Intensisade da luz ambiente
 
     # Vetores
@@ -21,14 +36,14 @@ def phong(entidade, luzes, ponto_intersec, camera_position):
         ]
     )
 
-    V = V / np.linalg.norm(V)  # Normaliza o vetor V
+    V /= np.linalg.norm(V)  # Normaliza o vetor V
+
+    N = None
 
     if isinstance(entidade, Esfera):
         N = np.array(
             entidade.__get_normal_vector_to_intersection_point__(ponto_intersec)
         )
-
-        N = N / np.linalg.norm(N)  # Normaliza a normal N
 
     elif isinstance(entidade, Plane):
         N = np.array(
@@ -38,8 +53,6 @@ def phong(entidade, luzes, ponto_intersec, camera_position):
                 entidade.normal.z,
             ]
         )
-
-        N = N / np.linalg.norm(N)  # Normaliza a normal N
 
     elif isinstance(entidade, Mesh):
         intersection_point = None
@@ -60,8 +73,8 @@ def phong(entidade, luzes, ponto_intersec, camera_position):
                     )
                     break
 
-        if intersection_point is not None:
-            N = N / np.linalg.norm(N)  # Normaliza a normal N
+    if N is not None:
+        N /= np.linalg.norm(N)  # Normaliza a normal N
 
     # ka * Ia é o ambiente (luz ambiente)
     # Para cada ponto de luz nós iremos rodar a parte da direita da equação
@@ -69,7 +82,6 @@ def phong(entidade, luzes, ponto_intersec, camera_position):
     # Il * ks * (R.dot(V)) ** n componente especular
 
     entidade.color = np.array(entidade.color)
-
     i_sum = np.array([0.0, 0.0, 0.0])
 
     for luz in luzes:
@@ -81,22 +93,23 @@ def phong(entidade, luzes, ponto_intersec, camera_position):
                 luz.z - ponto_intersec.z,
             ]
         )
-        L = L / np.linalg.norm(L)  # Normaliza o vetor L
+        L /= np.linalg.norm(L)  # Normaliza o vetor L
 
         R = 2 * N * (N.dot(L)) - L
-        R = R / np.linalg.norm(R)  # Normaliza o vetor R
+        R /= np.linalg.norm(R)  # Normaliza o vetor R
 
         N_dot_L = max(N.dot(L), 0)  # Garante que o produto escalar é não-negativo
         R_dot_V = max(R.dot(V), 0)  # Garante que o produto escalar é não-negativo
 
-        I_difusa = luz.I * entidade.color * entidade.k_difuso * N_dot_L
-        I_especular = luz.I * entidade.k_especular * (R_dot_V**entidade.n_rugosidade)
+        if N_dot_L > 0:
+            I_difusa = luz.I * entidade.color * entidade.k_difuso * N_dot_L
+            I_especular = (
+                luz.I * entidade.k_especular * (R_dot_V**entidade.n_rugosidade)
+            )
 
-        I = I_difusa + I_especular
-        i_sum += I
+            i_sum += I_difusa + I_especular
 
     cor = (entidade.k_ambiental * Ia) + i_sum
-
     cor_final = [min(255, max(0, int(i))) for i in cor]
 
     return cor_final
