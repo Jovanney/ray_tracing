aa45e47daa90fcca081c1fceb552c66e99a34fcd
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-08-20 10:50:23 -0300

    refactor: Update triangle mesh in main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """Main File"""
 
 import numpy as np
-from bsp import build_bsp
+from bsp import build_bsp, print_bsp_tree
 from vectors import Ponto, Vetor
-from entidades import Mesh, Esfera, Plane
+from entidades import Mesh, Plane
 from camera import Camera
 from ray_casting import RayCasting
 
@@ -31,30 +31,14 @@ def main():
     v4 = p5 - p3
     normal2 = v3.__cross__(v4).__normalize__()
 
-    # Define spheres
-    esfera_metalica = Esfera(
-        center=Ponto(0, -1, 3),
-        radius=1,
-        color=(255, 0, 0),
-    )
+    p6 = Ponto(0.5, -1, 1)
+    p7 = Ponto(1.5, -1, 1)
+    p8 = Ponto(1, 1, 1)
 
-    esfera_opaca = Esfera(
-        center=Ponto(2, -1, 3),
-        radius=1,
-        color=(0, 0, 255),
-    )
-
-    esfera_3 = Esfera(
-        center=Ponto(-2, -1, 3),
-        radius=1,
-        color=(0, 255, 0),
-    )
-
-    esfera_exemplo_2 = Esfera(
-        center=Ponto(0.2, 1, 3),
-        radius=0.7,
-        color=(0, 255, 0),
-    )
+    # Calculate normal for the third triangle
+    v5 = p7 - p6
+    v6 = p8 - p6
+    normal3 = v5.__cross__(v6).__normalize__()
 
     # Define camera
     camera = Camera(
@@ -63,33 +47,7 @@ def main():
         up=Vetor(0, 0, 1),
     )
 
-    # Define planes
-    plano = Plane(
-        point=Ponto(0, -1, 3),
-        normal=Vetor(0, 0, -1),
-        color=(255, 255, 0),
-    )
-
-    plano_atras = Plane(
-        point=Ponto(0, -1, 5),  # Behind the existing plane
-        normal=Vetor(0, 0, -1),
-        color=(255, 0, 255),
-    )
-
-    plano_frente = Plane(
-        point=Ponto(0, -1, 1),  # In front of the existing plane
-        normal=Vetor(0, 0, -1),
-        color=(0, 255, 255),
-    )
-
-    plano_meio = Plane(
-        point=Ponto(0, -1, 3),  # Intersecting the existing plane
-        normal=Vetor(1, 0, 0),  # Different normal to ensure intersection
-        color=(255, 255, 255),
-    )
-
     # Build BSP tree
-    # bsp_tree = build_bsp([plano, plano_atras, plano_frente, plano_meio])
 
     # print(bsp_tree)
 
@@ -117,12 +75,23 @@ def main():
         vertex_normals=[],
     )
 
-    # bsp_tree = build_bsp([triangle])
-    # Add entities
-    entidades = [triangle, triangle2]
+    triangle3 = Mesh(
+        triangle_quantity=1,
+        vertices_quantity=3,
+        vertices=[p6, p7, p8],
+        triangle_normals=[normal3],
+        color=(0, 255, 0),
+        triangle_tuple_vertices=[(0, 1, 2)],
+        vertex_normals=[],
+    )
 
+    entidades = [triangle, triangle2, triangle3]
+
+    root = build_bsp(entidades)
+
+    print_bsp_tree(root)
     # Generate image
-    ray_casting.__generate_image__(entidades, 1, camera)
+    # ray_casting.__generate_image__(entidades, 1, camera)
 
 
 main()
